2117e6421e8ad375bdd05a9bd9c0240e821b657e
danielaryass <daniel.aryass7@gmail.com>
2024-05-30 21:45:58 +0700

    chore: test

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -3,6 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest, response: NextResponse) {
   const ip = request.headers.get("x-forwarded-for");
   const ip2 = request.ip;
-
+  console.log(ip);
   return NextResponse.json({ daniel: ip, arya: ip2 });
 }
